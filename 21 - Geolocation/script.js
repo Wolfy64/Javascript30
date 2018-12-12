c16ea5ed8a02fa691eebca1d913fadaace74370b
David De Wulf <contact.dewulf@gmail.com>
2018-12-11 17:48:51 -0800

    21 - Geolocation

---
+++ 21 - Geolocation/script.js
@@ -0,0 +1,12 @@
+console.log('SCRIPT');
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed-value');
+
+navigator.geolocation.watchPosition(
+  data => {
+    console.log(data);
+    speed.textContent = data.coords.speed;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  },
+  err => console.error('Hey you not allow to happend !!!')
+);
